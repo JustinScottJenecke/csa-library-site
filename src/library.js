534a03b6d78088d3e6a025385b4138ccef7570cb
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2022-02-16 23:00:49 +0200

    adds basic and markup to library.html, create Vue instance in library.js

---
+++ src/library.js
@@ -0,0 +1,21 @@
+const libraryApp = new Vue({
+
+    el : "#library-app",
+
+    data : {
+
+        // aside btn options
+        navOptions : [
+            "Records",
+            "Add",
+            "Search"
+        ],
+
+        // prop for search modeal
+        searchCriteria : "",
+
+        // prop 
+
+    }
+
+})
