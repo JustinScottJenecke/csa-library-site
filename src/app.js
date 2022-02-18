d98398bc4c49833f79f9decec47b381e3e00f7d5
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2022-02-18 10:19:09 +0200

    creates functional login system, saves user to storage, ensures singleton

--- src/app.js
+++ src/app.js
@@ -1,6 +1,29 @@
-/* ---- Imports and downloads ---- */
 
-import { userTable } from "./database/users.js";
 
-console.log(userTable)
+// ------------Constants-----------------//
+    
+    // key for users in storage
+    const userDB = "csa-auth-userDatabase"
 
+    // key for records in storage
+    const recordDB = "csa-auth-recordsDatabase"
+
+
+    let users = JSON.parse(localStorage.getItem(userDB));
+    console.log(users)
+
+// // Mock singleton - Make sure userDatabase is only created once in Storage api
+//     if (!localStorage.getItem(userDB)) {
+
+//         console.log("Please create user database")
+//         let users = JSON.stringify(userTable);
+
+//         localStorage.setItem(userDB, users);
+//         console.log(localStorage.getItem(userDB));
+//         console.log("db created successfully")
+        
+//     } else {
+
+//         console.log("user database already exists")
+//         console.log(localStorage.getItem(userDB));
+//     }
--- src/scripts/login.js
+++ src/scripts/login.js
@@ -9,17 +9,44 @@ let ACTIVE_USER_ROLE;
 // User Database
 import { userTable } from "../database/users.js";
 
-console.log(userTable)
+let users = JSON.stringify(userTable);
+console.log(users)
 
 // Get form sumbit buttons
 
 const loginOption = document.getElementById("login-action");
 const signUpOption = document.getElementById("signup-action");
+    
+// key for users in storage
+const userDB = "csa-auth-userDatabase"
 
 /*---------------------------
     Login
 -----------------------------*/
+
+const loginDatabaseFunctionality = () => {
+
+// Mock singleton - Make sure userDatabase is only created once in Storage api
+    if (!localStorage.getItem(userDB)) {
+
+        console.log("Please create user database")
+
+        localStorage.setItem(userDB, users);
+        console.log(localStorage.getItem(userDB));
+        console.log("db created successfully")
+        
+    } else {
+
+        console.log("user database already exists")
+        console.log(localStorage.getItem(userDB));
+    }
+}
+
+
 const loginFunction = () => {
+
+    // controller exists variable
+    let authenticated = false;
 
     // getDataFromFields
     let username = document.getElementById("username-login").value;
@@ -33,15 +60,25 @@ const loginFunction = () => {
             ACTIVE_USERNAME = username;
             ACTIVE_USER_ROLE = user.role;
 
-            localStorage.setItem("csa-auth-user", ACTIVE_USERNAME);
-            localStorage.setItem("csa-auth-role", ACTIVE_USER_ROLE);
-
-            // redirect user to app
-            window.location = ("./src/library.html")
-            
+            authenticated = true;
         }
 
     });
+
+    if (authenticated) {
+        
+        localStorage.setItem("csa-auth-user", ACTIVE_USERNAME);
+        localStorage.setItem("csa-auth-role", ACTIVE_USER_ROLE);
+
+        loginDatabaseFunctionality();
+
+        // redirect user to app
+        window.location = ("./src/library.html")
+        
+    } else {
+
+        alert("Invalid username or password. Please sign in using your correct account details");
+    }
 
 };
 
@@ -69,11 +106,20 @@ const signupFunction = () => {
             
         };
 
-        // add user to database
-        userTable.push(newUser)
+        loginDatabaseFunctionality();
+
+        // --- add user to database --- //
+       
+        // add newUser to js array
+        users = JSON.parse(users);
+        users.push(newUser);
 
         console.log("Updated succesfully!")
-        console.log(userTable)
+        console.log(users)
+
+        // convert js array to JSON and save to storage
+        users = JSON.stringify(users);
+        localStorage.setItem(userDB, users)
 
         // log new user in
         ACTIVE_USERNAME = newUser.username;
